8ad7bd6d6669a680380a7451727fd6fa303fdc01
arunk28 <arunkumar280491@gmail.com>
2020-11-22 11:03:55 +0530

    controlled component excersise

--- src/Component/Search.js
+++ src/Component/Search.js
@@ -1,14 +1,28 @@
-import React from "react";
 
-const Search = () => {
-  return (<div className="ui segment">
-      <form className="ui form">
-          <div className="field">
-              <label> Image Search</label>
-              <input type="text"></input>
-          </div>
-      </form>
-      </div>);
-};
+import React, { Component } from 'react';
+class Search extends Component {
+    state = { term : 'Hi theres'  }
 
+    //method of event handler
+    //if more than one line of code we can use function
+    onInputChange =  (event)=>{
+        console.log(event.target.value)
+    }
+
+    render() { 
+        return ( 
+            <div className="ui segment">
+        <form className="ui form">
+            <div className="field">
+                <label> Image Search</label>
+                {/* <input type="text" onChange={this.onInputChange}></input> */}
+                {/* <input type="text" onChange={(event)=>{console.log(event.target.value)}}></input> */}
+                <input type="text" value={this.state.term} onChange={e=>{this.setState({term:e.target.value})}}></input>
+            </div>
+        </form>
+        </div>); 
+    }
+}
+ 
 export default Search;
+ 
