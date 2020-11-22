1b5a6fd2c7b61172a4f9f25f1d35f28c4a57e3ac
arunk28 <arunkumar280491@gmail.com>
2020-11-22 19:55:24 +0530

    convert function to class component

--- src/Component/App.js
+++ src/Component/App.js
@@ -1,9 +1,18 @@
-import React from "react";
+import React,{Component} from "react";
 import Search from "./Search";
 
-const App = () => {
-  return (<div className="ui container" style={{marginTop:'10px'}}>
-    <Search /></div>);
-};
+class App extends Component {
+
+ onSearchSubmit(term){
+   console.log(term);
+ }
+ 
+  render() { 
+    return   (<div className="ui container" style={{marginTop:'10px'}}>
+    <Search /></div> );
+  }
+}
+ 
+
 
 export default App;
