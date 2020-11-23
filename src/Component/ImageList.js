0a2353b6c22349c785bc283174ceee5cdee58baf
arunk28 <arunkumar280491@gmail.com>
2020-11-23 21:50:37 +0530

    grid css implemented

--- src/Component/ImageList.js
+++ src/Component/ImageList.js
@@ -1,10 +1,12 @@
+
+import './ImageList.css';
 import React from "react";
 
 const ImageList = (props) => {
   const images = props.images.map(({cover_photo}) => {
     return <img key={cover_photo.id} alt={cover_photo.alt_description} src={cover_photo.urls.regular}></img>;
   });
-  return <div>{images}</div>;
+  return <div className="image-list">{images}</div>;
 };
 
 export default ImageList;
