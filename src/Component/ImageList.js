6b2cbaf199960290c84849a50933b5da1ecec5f9
arunk28 <arunkumar280491@gmail.com>
2020-11-23 21:39:21 +0530

    image list excersise

--- src/Component/ImageList.js
+++ src/Component/ImageList.js
@@ -1,8 +1,8 @@
 import React from "react";
 
 const ImageList = (props) => {
-  const images = props.images.map((image) => {
-    return <img src={image.cover_photo.urls.regular}></img>;
+  const images = props.images.map(({cover_photo}) => {
+    return <img key={cover_photo.id} alt={cover_photo.alt_description} src={cover_photo.urls.regular}></img>;
   });
   return <div>{images}</div>;
 };
