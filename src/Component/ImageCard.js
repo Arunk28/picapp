9cf828a6bd557dce5f0199649df9be48c7bcce9a
arunk28 <arunkumar280491@gmail.com>
2020-11-24 10:43:34 +0530

    style in dynamic mode

--- src/Component/ImageCard.js
+++ src/Component/ImageCard.js
@@ -1,10 +1,27 @@
 import React, { Component } from 'react';
 
 class ImageCard extends Component {
-   
+   constructor(props){
+       super(props);
+       this.imageRef = React.createRef();
+   }
+   state ={span : 0};
+   //before load we cant access imag elemnent 
+   //so we added event listenter attached to get the height of the element
+   componentDidMount(){
+       this.imageRef.current.addEventListener('load',this.setSpan);
+   }
+   setSpan = ()=>{
+       const height =this.imageRef.current.clientHeight;
+        this.setState({span : height});
+   }
     render() { 
         const {id ,alt_description,urls }  = this.props.searchImages;
-        return ( <img key={id} alt={alt_description} src={urls.regular}></img>  );
+        return ( 
+        <div style={{ gridRowEnd :  `span ${this.state.span}`}}>
+            <img ref={this.imageRef} key={id} alt={alt_description} src={urls.regular}></img>  
+        </div>
+        );
     }
 }
  
--- src/Component/ImageList.js
+++ src/Component/ImageList.js
@@ -5,7 +5,7 @@ import ImageCard from './ImageCard';
 
 const ImageList = (props) => {
   const images = props.images.map(({cover_photo}) => {
-    return <ImageCard searchImages = {cover_photo} />;
+    return <ImageCard key={cover_photo.id} searchImages = {cover_photo} />;
   });
   return <div className="image-list">{images}</div>;
 };
